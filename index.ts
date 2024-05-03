048d1beb03e2719ecaaf3182789c8c1664e0755d
saleem-301 <adnanyousuf301@gmail.com>
2024-05-04 01:14:54 +0500

    project student system

---
+++ index.ts
@@ -0,0 +1,220 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk'
+import EditorPrompt from "inquirer/lib/prompts/editor.js";
+let stuData: { [key: string]: any } = [];
+
+let courses: any = {
+  "Ms Office": 10000,
+  Html: 20000,
+  "C++": 9000,
+  "Java Script": 14000,
+  "Type Script": 15000,
+};
+
+
+let studID = 0;
+console.log(chalk.yellow ("\n\nWelcome to Pak Institute\n"));
+
+
+for (let i = 1; i != 0; ) {
+  let status = await inquirer.prompt({
+    name: "status",
+    type: "list",
+    message:chalk.green ( "\nWhat Would you Like to Do\n"),
+    choices: [
+      "Edit Existing Student's Record",
+      "Check Student's Status",
+      "Enrol New Student",
+    ],
+  });
+  console.log(status.status);
+
+  //CONDITION 1 : TO ENROL NEW STUDENT
+
+  if (status.status === "Enrol New Student") {
+    let studInfo = await inquirer.prompt(
+      [
+      {
+        name: "name",
+        type: "input",
+        message: chalk.cyanBright ("Enter Your Name"),
+      },
+      {
+        name: "fname",
+        type: "input",
+        message: chalk.cyanBright("Enter Your father's name",)
+      },
+      {
+        name: "Course",
+        type: "list",
+        message:chalk.cyanBright( "Select Your Course to Enrol"),
+        choices: ["Ms Office", "HTML", "C++", "Java Script", "Type Script"]
+      },
+      {
+        name: "Fee",
+        type: "list",
+        message: "Select Status of Fee",
+        choices:["Paid", "Unpaid", "Partialy Paid"],
+      },
+      ]);
+    //Gerneratinig Students ID and storing data in to stuData
+    studID++;
+    let students: { [key: string]: any } = {
+      ID: studID.toString().padStart(5, "0"),
+      Name: studInfo.name,
+      Fname: studInfo.fname,
+      Course: studInfo.Course,
+      Fee: studInfo.Fee,
+    };
+    stuData[stuData.length] = students;
+
+    //STORING FEE AND BALANCE 
+    let lastVal = stuData[stuData.length - 1];
+
+    if (lastVal.Fee === "Unpaid") 
+      {
+        lastVal.Balance = courses[lastVal.Course];
+      }
+    else if (stuData[stuData.length - 1].Fee === "Partialy Paid") 
+      {
+        let amt = await inquirer.prompt(
+          {
+            name: "payment",
+            type: "input",
+            message: "\nEnter the amount Paid\n",
+          });
+      // console.log(amt.payment)
+      stuData[stuData.length - 1]["Balance"] =
+        courses[stuData[stuData.length - 1]["Course"]] - amt.payment;
+      }
+    else 
+      { stuData[stuData.length - 1]["Balance"] = 0; }
+  }
+  //CONDITION 2 : TO CHECK STATUS
+  else if (status.status === "Check Student's Status")
+  {
+    if (stuData.length === 0) 
+      {
+        console.log(`\nThere is no Record in Data\n`);
+      }
+    else 
+      {
+        console.log(stuData);
+      }
+  }
+  //CONDITION 3 : TO EDIT EXISTING STUDENT'S RECORD
+
+  else if (status.status === "Edit Existing Student's Record") {
+    let exId = await inquirer.prompt([
+      {
+        name: "exId",
+        type: "input",
+        message: "\n Type student's ID you want to Edit \n",//asking for id to change record
+      },
+      
+    ]);
+    console.log(exId.exId)
+
+
+    //LOOP TO CHECK EACH RECORD TO FIND DESIRED ID RECORD
+    for (let i = 0; i<stuData.length; i++) 
+    {console.log(stuData[i].ID)
+
+      if (stuData[i].ID === exId.exId) 
+
+      {
+        console.log(`Found your Desired ID ${exId.exId}`)
+        let newfield = await inquirer.prompt
+        ({
+          name: "field",
+          type: "list",
+          message: "\nSelect the field you want to edit",
+          choices: ["Name", "Fname", "Course", "Fee"],
+        })
+        //Editing options other than fee
+        if (newfield.field != "Fee") {
+          let newVal = await inquirer.prompt({
+            name: "newName",
+            type: "input",
+            message: "Enter New Value",
+          });
+console.log(newVal.newName)
+          stuData[i][newfield.field] = newVal.newName;
+        }
+        //EDITING THE FEE OPTION
+        else {
+          let newFee = await inquirer.prompt({
+            name: "newFee",
+            type: "list",
+            message: "Select New Value",
+            choices: ["Paid", "Unpaid", "Partialy Paid"],
+          });
+          stuData[i][newfield.field] = newFee.newFee;
+
+          if (newFee.newFee === "Unpaid") {
+            stuData[i].Balance = courses[stuData[i].Course];
+          } 
+          else if (newFee.newFee === "Partialy Paid") 
+            {
+            let amt = await inquirer.prompt({
+              name: "payment",
+              type: "input",
+              message: "\nEnter the amount Paid\n",
+            });
+
+            
+            stuData[i].Balance = courses[stuData[i].Course] - amt.payment;
+          }
+          else 
+          {
+            stuData[i].Balance = 0;
+          }
+        }      
+      console.log(stuData[i]);
+        break
+    }
+      else if(i === stuData.length-1 ) 
+        {
+          console.log("ID DOES NOT EXIST");
+          break
+        }  
+    
+      // IF ID DOESN'T EXIST
+      else 
+      {console.log(i)
+        continue;
+      }
+  }
+      
+       
+      
+    
+  
+  let Trans = await inquirer.prompt
+  ({
+    name: "moreTrans",
+    type: "list",
+    message: "\nDo you want to perform more Transactions\n",
+    choices: ["Yes", "No"],
+  });
+  if (Trans.moreTrans === "No") 
+    {
+      i = 0;
+
+    }
+    else
+    {
+    continue;
+    }
+
+  }}
+  
+
+console.log(stuData);
+
+//errors
+//after entering record not asking for more trans
+//on partial paid balance shows 0
+
+
---
+++ test.js
@@ -0,0 +1,21 @@
+import inquirer from 'inquirer';
+let id = [{ id: '00001', name: 'saleem' }, { id: '00002', name: 'Hammad' }];
+// let n : [string] ='name' 
+let newVal = await inquirer
+    .prompt({
+    name: "newName",
+    type: "list",
+    message: "Enter New Value",
+    choices: ['id', 'name']
+});
+let news = await inquirer
+    .prompt({
+    name: "news",
+    type: "input",
+    message: "Enter New Value",
+    //   choices: ['id','name']
+});
+console.log(id[0][newVal.newName]);
+console.log(news.news);
+id[0][newVal.newName] = news.news;
+console.log(id);
